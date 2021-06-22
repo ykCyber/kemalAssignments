858bfca993606d2820508fbb73d059478f323fba
mustafa <yusufkenan2604@gmail.com>
2021-06-23 00:21:37 +0200

    sec

---
+++ src/telephone/Test.java
@@ -0,0 +1,53 @@
+package telephone;
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        MobilePhone nokia = new MobilePhone();
+                new MobilePhone();
+       nokia=  new iPhone("12");
+        Smartphone sm = new iPhone("13");
+        ((iPhone)nokia).music();
+        System.out.println(nokia.hashCode());
+               //  nokia = new Android("samsung", "10");
+
+
+       // nokia.call("ömer");
+
+//        iPhone xMax = new iPhone("12");
+//        ( nokia) = new iPhone("123");
+
+//        (nokia).music();
+//        mp = new iPhone("13");
+
+
+//        if( mp instanceof Android){
+//            System.out.println("android");
+//        }
+//        else{
+//            System.out.println("other");
+    }
+//		mp = new MobilePhone();
+//		mp = new Android("Samsung","Android 10");
+//        mp.navigation();
+
+//        mp = new iPhone("13.2");
+//		mp.music();
+//		((iPhone) mp).music();
+//		mp.call(" sam");
+//		((Smartphone)mp).navigation("job");
+//		 mp.navigation();
+//		(mp).appStore();.,
+//		((Smartphone)mp).navigation("home");
+//		MobilePhone mp1 = new iPhone("12");
+
+
+}
+
+
